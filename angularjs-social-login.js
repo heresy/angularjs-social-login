62dac24563c80e1b57fa35c86a605fb45055800b
Jayaram K <kurapatijayaram@gmail.com>
2015-12-11 01:20:48 +0530

    replaced watchers with broadcast events for binding events after scripts were loaded.

--- angularjs-social-login.js
+++ angularjs-social-login.js
@@ -3,10 +3,6 @@
 var socialLogin = angular.module('socialLogin', []);
 
 socialLogin.run(function($window, $rootScope){
-	$rootScope.fbReady = false;
-	$rootScope.googleReady = false;
-	$rootScope.linkedINReady = false;
-
 	$window.googleSignInCallback = function(authResult){
 		if(authResult.status.signed_in && authResult.status.method == "PROMPT"){
 			$rootScope.$broadcast('event:google-sign-in-success', authResult);
@@ -22,7 +18,7 @@ socialLogin.run(function($window, $rootScope){
 		lIN.src = "//platform.linkedin.com/in.js?async=false";
 		
 		lIN.onload = function() {
-			$rootScope.linkedINReady = true;
+			$rootScope.$broadcast('event:social-login-linkedIn-loaded', true);
 		};
 		lIN.text = ("api_key: " + apiKey).replace("\"", "");
         ref.parentNode.insertBefore(lIN, ref);
@@ -36,7 +32,7 @@ socialLogin.run(function($window, $rootScope){
 		fbJs.src = "//connect.facebook.net/en_US/sdk.js";
 
 		fbJs.onload = function() {
-			$rootScope.fbReady = true;
+			$rootScope.$broadcast('event:social-login-fb-loaded', true);
         };
 
 		ref.parentNode.insertBefore(fbJs, ref);
@@ -49,7 +45,7 @@ socialLogin.run(function($window, $rootScope){
 		gJs.src = "https://apis.google.com/js/client:platform.js"
 
 		gJs.onload = function() {
-			$rootScope.googleReady = true;
+			$rootScope.$broadcast('event:social-login-google-loaded', true);
         };
 
         ref.parentNode.insertBefore(gJs, ref);
@@ -107,20 +103,18 @@ socialLogin.directive("linkedIn", function($rootScope, social){
 		scope: {},
 		link: function(scope, ele, attr){
 			$rootScope.addLinkedInScript(social.linkedInKey);
-			$rootScope.$watch('linkedINReady', function(nVal, oVal){
-				if(nVal){
-					ele.on("click", function(){
-						IN.User.authorize(function(){});
-					})
+			$rootScope.$on('event:social-login-linkedIn-loaded', function(event, status){
+			    ele.on("click", function(){
+					IN.User.authorize(function(){});
+				})
 
-					IN.Event.on(IN, "auth", function(){
-						IN.API.Raw("/people/~:(id,first-name,last-name,email-address)").result(function(res){
-							var userDetails = {name: res.firstName + " " + res.lastName, email: res.emailAddress, uid: res.id, provider: "linkedIN"}
-							$rootScope.$broadcast('event:social-sign-in-success', userDetails);
-					    });
-					})
-				}
-			})
+				IN.Event.on(IN, "auth", function(){
+					IN.API.Raw("/people/~:(id,first-name,last-name,email-address)").result(function(res){
+						var userDetails = {name: res.firstName + " " + res.lastName, email: res.emailAddress, uid: res.id, provider: "linkedIN"}
+						$rootScope.$broadcast('event:social-sign-in-success', userDetails);
+				    });
+				})
+			 });
 		}
 	}
 })
@@ -163,39 +157,38 @@ socialLogin.directive("gLogin", function($rootScope, social){
 	}
 });
 
-socialLogin.directive("fbLogin", function($rootScope, fbService, $cookieStore, $location, social){
+socialLogin.directive("fbLogin", function($rootScope, fbService, social){
 	return {
 		restrict: 'A',
 		scope: {},
 		replace: true,
 		link: function(scope, ele, attr){
 			$rootScope.addFbScript();
-			$rootScope.$watch(function(){return $rootScope.fbReady}, function(nVal, oVal){
-				if(nVal){
-					FB.init({ 
-						appId: social.fbKey,
-						status: true, 
-						cookie: true, 
-						xfbml: true,
-						version: 'v2.4'
+			$rootScope.$on('event:social-login-fb-loaded', function(event, status){
+				FB.init({ 
+					appId: social.fbKey,
+					status: true, 
+					cookie: true, 
+					xfbml: true,
+					version: 'v2.4'
+				});
+
+				ele.on('click', function(){
+					fbService.login().then(function(res){
+						if(res.status == "connected"){
+							fbService.getUserDetails().then(function(res){
+								var userDetails = {email: res.email, uid: res.id, provider: "facebook"}
+								$rootScope.$broadcast('event:social-sign-in-success', userDetails);
+							}, function(err){
+
+							})
+						}
+					}, function(err){
+
 					});
-				}
-			});
-			
-			ele.on('click', function(){
-				fbService.login().then(function(res){
-					if(res.status == "connected"){
-						fbService.getUserDetails().then(function(res){
-							var userDetails = {email: res.email, uid: res.id, provider: "facebook"}
-							$rootScope.$broadcast('event:social-sign-in-success', userDetails);
-						}, function(err){
-
-						})
-					}
-				}, function(err){
-
 				});
 			});
+			
 		}
 	}
 })
